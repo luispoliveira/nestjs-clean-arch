3fd18610c99cff6dd1ed56ed46f130a4784c3ea0
Luis Oliveira <luispedroliveira96@gmail.com>
2025-07-30 22:05:31 +0100

    test: update jest configuration to include integration test files for ClassValidatorFields

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ export default {
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
-  testRegex: '.*\\.spec\\.ts$',
+  testRegex: '.*\\.(spec|int-spec)\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
