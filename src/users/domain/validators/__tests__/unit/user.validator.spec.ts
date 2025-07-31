1275bcd5398c7f01f12bd23e1a150df1f21c95a4
Luis Oliveira <luispedroliveira96@gmail.com>
2025-07-31 10:03:53 +0100

    feat: enhance UserValidator unit tests with additional validation cases for email, password, and createdAt fields

--- src/users/domain/validators/__tests__/unit/user.validator.spec.ts
+++ src/users/domain/validators/__tests__/unit/user.validator.spec.ts
@@ -1,12 +1,25 @@
+import { UserProps } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
-import { UserValidator, UserValidatorFactory } from '../../user.validator'
+import {
+  UserRules,
+  UserValidator,
+  UserValidatorFactory,
+} from '../../user.validator'
 
 let sut: UserValidator
+let props: UserProps
 describe('UserValidator unit tests', () => {
   beforeEach(() => {
     sut = UserValidatorFactory.create()
+    props = UserDataBuilder({})
   })
+  it('Valide cases for user rules', () => {
+    const isValid = sut.validate(props)
 
+    expect(isValid).toBeTruthy()
+    expect(sut.errors).toBeNull()
+    expect(sut.validatedData).toStrictEqual(new UserRules(props))
+  })
   describe('Name field', () => {
     it('Invalidation cases for name field', () => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
@@ -38,6 +51,99 @@ describe('UserValidator unit tests', () => {
       expect(sut.errors).not.toBeNull()
       expect(sut.errors!['name']).toStrictEqual([
         'name must be shorter than or equal to 255 characters',
+      ])
+    })
+  })
+
+  describe('Email field', () => {
+    it('Invalidation cases for naemailme field', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      let isValid = sut.validate(null as any)
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      console.log("🚀 ~ sut.errors!['email']:", sut.errors!['email'])
+
+      expect(sut.errors!['email']).toStrictEqual([
+        'email must be an email',
+        'email must be a string',
+        'email should not be empty',
+      ])
+
+      isValid = sut.validate({ ...UserDataBuilder({}), email: '' })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['email']).toStrictEqual([
+        'email must be an email',
+        'email should not be empty',
+      ])
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      isValid = sut.validate({ ...UserDataBuilder({}), email: 10 as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['email']).toStrictEqual([
+        'email must be an email',
+        'email must be a string',
+      ])
+    })
+  })
+
+  describe('Password field', () => {
+    it('Invalidation cases for password field', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      let isValid = sut.validate(null as any)
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['password']).toStrictEqual([
+        'password must be shorter than or equal to 100 characters',
+        'password must be a string',
+        'password should not be empty',
+      ])
+
+      isValid = sut.validate({ ...UserDataBuilder({}), password: '' })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['password']).toStrictEqual([
+        'password should not be empty',
+      ])
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      isValid = sut.validate({ ...UserDataBuilder({}), password: 10 as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['password']).toStrictEqual([
+        'password must be shorter than or equal to 100 characters',
+        'password must be a string',
+      ])
+
+      isValid = sut.validate({
+        ...UserDataBuilder({}),
+        password: 'a'.repeat(101),
+      })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['password']).toStrictEqual([
+        'password must be shorter than or equal to 100 characters',
+      ])
+    })
+  })
+
+  describe('CreatedAt field', () => {
+    it('Invalidation cases for createdAt field', () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      let isValid = sut.validate({ ...props, createdAt: 10 as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['createdAt']).toStrictEqual([
+        'createdAt must be a Date instance',
+      ])
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      isValid = sut.validate({ ...props, createdAt: '2023' as any })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors).not.toBeNull()
+      expect(sut.errors!['createdAt']).toStrictEqual([
+        'createdAt must be a Date instance',
       ])
     })
   })
