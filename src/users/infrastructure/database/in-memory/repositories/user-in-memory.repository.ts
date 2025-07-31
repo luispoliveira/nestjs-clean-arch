64823d2b8d3111cabcd1d20a61b142cd0b2eafc0
Luis Oliveira <luispedroliveira96@gmail.com>
2025-07-31 15:16:33 +0100

    feat: implement UserInMemoryRepository with findByEmail and emailExists methods

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,24 @@
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+
+export class UserInMemoryRepository
+  extends InMemoryRepository<UserEntity>
+  implements UserRepository
+{
+  async findByEmail(email: string): Promise<UserEntity> {
+    const user = this.items.find(user => user.email === email)
+    if (!user) throw new NotFoundError(`User with email ${email} not found`)
+
+    return Promise.resolve(user)
+  }
+
+  async emailExists(email: string): Promise<void> {
+    const user = this.items.find(user => user.email === email)
+    if (user) throw new ConflictError(`User with email ${email} already exists`)
+
+    return Promise.resolve()
+  }
+}
