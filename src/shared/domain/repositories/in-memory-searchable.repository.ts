747eed69698d8a292515bd6ee19937f170509d8f
Luis Oliveira <luispedroliveira96@gmail.com>
2025-07-31 17:26:54 +0100

    feat: update InMemorySearchableRepository to use SearchParams in search method and add pagination logic

--- src/shared/domain/repositories/in-memory-searchable.repository.ts
+++ src/shared/domain/repositories/in-memory-searchable.repository.ts
@@ -1,12 +1,38 @@
 import { Entity } from '../entities/entity'
 import { InMemoryRepository } from './in-memory.repository'
-import { SearchableRepositoryInterface } from './searchable-repository-contracts'
+import {
+  SearchableRepositoryInterface,
+  SearchParams,
+  SearchResult,
+  SortDirection,
+} from './searchable-repository-contracts'
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  search(input: any): Promise<any> {
+  async search(input: SearchParams): Promise<SearchResult<E>> {
     throw new Error('Method not implemented.')
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>
+
+  protected async applySort(
+    items: E[],
+    sort: string | null,
+    sortDirection: SortDirection | null,
+  ): Promise<E[]> {}
+
+  protected async applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {
+    const start = (page - 1) * perPage
+    const end = start + perPage
+    return items.slice(start, end)
   }
 }
