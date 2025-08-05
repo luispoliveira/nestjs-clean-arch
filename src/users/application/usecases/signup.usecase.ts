a275b192e5002c601fe8616fc76b2db2762a1291
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-05 14:50:08 +0100

    feat: implement SignupUseCase with input and output types

---
+++ src/users/application/usecases/signup.usecase.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SignupUseCase {
+  export type Input = {
+    name: string
+    email: string
+    password: string
+  }
+
+  export type Output = {
+    id: string
+    name: string
+    email: string
+    password: string
+    createdAt: Date
+  }
+
+  export class UseCase {
+    async execute(input: Input): Promise<Output> {}
+  }
+}
