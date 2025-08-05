75a9fb03f023a0b9c261e6140d4bc44eacd7583c
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-05 15:42:50 +0100

    test: add unit tests for SignupUseCase to validate user signup functionality

---
+++ src/users/application/usecases/__tests__/unit/signup.usecase.spec.ts
@@ -0,0 +1,51 @@
+import { HashProvider } from '@/shared/application/providers/hash.provider'
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
+import { BcryptjsHashProvider } from '@/users/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
+import { SignupUseCase } from '../../signup.usecase'
+
+describe('SignupUseCase unit test', () => {
+  let sut: SignupUseCase.UseCase
+  let repository: UserInMemoryRepository
+  let hasProvider: HashProvider
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository()
+    hasProvider = new BcryptjsHashProvider()
+    sut = new SignupUseCase.UseCase(repository, hasProvider)
+  })
+
+  it('should sign up a user successfully', async () => {
+    const spyInsert = jest.spyOn(repository, 'insert')
+    const props = UserDataBuilder({})
+
+    const result = await sut.execute({
+      name: props.name,
+      email: props.email,
+      password: props.password,
+    })
+
+    expect(result.id).toBeDefined()
+    expect(result.createdAt).toBeInstanceOf(Date)
+    expect(spyInsert).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw an error if name, email, or password is missing', async () => {
+    await expect(
+      sut.execute({ name: '', email: '', password: '' }),
+    ).rejects.toThrow('Name, email, and password are required.')
+  })
+
+  it('should throw an error if email already exists', async () => {
+    const props = UserDataBuilder({ email: 'john@example.com' })
+
+    await sut.execute({
+      name: props.name,
+      email: props.email,
+      password: props.password,
+    })
+
+    await expect(() => sut.execute(props)).rejects.toBeInstanceOf(ConflictError)
+  })
+})
