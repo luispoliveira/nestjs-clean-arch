710c8eeb06515dc0d53ac842e7e63f19a0287b4b
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-05 15:06:03 +0100

    feat: implement BadRequestError and integrate it into SignupUseCase for input validation

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,3 +1,7 @@
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { BadRequestError } from '../errors/bad-request-error'
+
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace SignupUseCase {
   export type Input = {
@@ -15,6 +19,21 @@ export namespace SignupUseCase {
   }
 
   export class UseCase {
-    async execute(input: Input): Promise<Output> {}
+    constructor(private userRepository: UserRepository.Repository) {}
+
+    async execute(input: Input): Promise<Output> {
+      const { name, email, password } = input
+
+      if (!name || !email || !password)
+        throw new BadRequestError('Name, email, and password are required.')
+
+      await this.userRepository.emailExists(email)
+
+      const entity = new UserEntity(input)
+
+      await this.userRepository.insert(entity)
+
+      return entity.toJSON()
+    }
   }
 }
