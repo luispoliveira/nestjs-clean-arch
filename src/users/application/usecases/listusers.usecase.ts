90a265c1eeae341d39619a0941e34e0aa6f9c18c
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-06 15:57:22 +0100

    feat: implement pagination output DTO and update ListUsersUseCase to return paginated user results

--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,12 +1,17 @@
+import {
+  PaginationOutputDTO,
+  PaginationOutputMapper,
+} from '@/shared/application/dtos/pagination-output.dto'
 import { SearchInputDTO } from '@/shared/application/dtos/search-input.dto'
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserOutputDTO, UserOutputMapper } from '../dtos/user-output.dto'
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace ListUsersUseCase {
   export type Input = SearchInputDTO
 
-  export type Output = void
+  export type Output = PaginationOutputDTO<UserOutputDTO>
 
   export class UseCase implements DefaultUseCase<SearchInputDTO, Output> {
     constructor(private userRepository: UserRepository.Repository) {}
@@ -14,6 +19,15 @@ export namespace ListUsersUseCase {
     async execute(input: SearchInputDTO): Promise<Output> {
       const params = new UserRepository.SearchParams(input)
       const searchResult = await this.userRepository.search(params)
+
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item)
+      })
+      return PaginationOutputMapper.toOutput(items, searchResult)
     }
   }
 }
