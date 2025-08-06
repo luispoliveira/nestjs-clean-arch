9ab7b1787ec05510d27a1e3388965e7e966b6adf
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-06 16:30:30 +0100

    feat: add unit tests for PaginationOutputDTO and UserOutputDTO mappers

---
+++ src/users/application/dtos/__tests__/unit/user-output.dto.spec.ts
@@ -0,0 +1,14 @@
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserOutputMapper } from '../../user-output.dto'
+
+describe('UserOutputDTO unit test', () => {
+  it('should convert a user in output', () => {
+    const entity = new UserEntity(UserDataBuilder({}))
+    const spyToJson = jest.spyOn(entity, 'toJSON')
+    const sut = UserOutputMapper.toOutput(entity)
+
+    expect(spyToJson).toHaveBeenCalledTimes(1)
+    expect(sut).toStrictEqual(entity.toJSON())
+  })
+})
