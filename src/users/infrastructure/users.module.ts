a2a524be65b7923468a34b10464d98e5f9386e42
Luis Oliveira <luispedroliveira96@gmail.com>
2025-08-06 22:32:02 +0100

    feat: enhance UsersModule by adding UserRepository and HashProvider providers for SignUpUseCase

--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,9 +1,34 @@
+import { HashProvider } from '@/shared/application/providers/hash.provider'
 import { Module } from '@nestjs/common'
+import { SignUpUseCase } from '../application/usecases/sign-up.usecase'
+import { UserRepository } from '../domain/repositories/user.repository'
+import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
+import { BcryptjsHashProvider } from './providers/hash-provider/bcryptjs-hash.provider'
 import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    {
+      provide: SignUpUseCase.UseCase,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SignUpUseCase.UseCase(userRepository, hashProvider)
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class UsersModule {}
