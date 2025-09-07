51a51a9e72283850d2ef7e6cdef5ba7ba7b105b3
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-07 17:41:26 +0100

    feat: add integration tests for GetUserUseCase

---
+++ src/users/application/usecases/__tests__/integration/get-user.usecase.int-spec.ts
@@ -0,0 +1,58 @@
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma.repository'
+import { PrismaClient } from '@generated/prisma'
+import { Test, TestingModule } from '@nestjs/testing'
+import { GetUserUseCase } from '../../get-user.usecase'
+
+describe('GetUserUsecase Integration Tests', () => {
+  const prismaService = new PrismaClient()
+  let sut: GetUserUseCase.UseCase
+  let repository: UserPrismaRepository
+
+  let module: TestingModule
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile()
+    await prismaService.$connect()
+    repository = new UserPrismaRepository(prismaService as any)
+  })
+
+  beforeEach(async () => {
+    sut = new GetUserUseCase.UseCase(repository)
+    await prismaService.user.deleteMany()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  it('slould throw an error when user not found', async () => {
+    const props: GetUserUseCase.Input = {
+      id: 'non-existing-id',
+    }
+
+    await expect(() => sut.execute(props)).rejects.toThrow(
+      new Error('User with id non-existing-id not found'),
+    )
+  })
+
+  it('should return a user', async () => {
+    const entity = new UserEntity(UserDataBuilder({}))
+
+    await repository.insert(entity)
+
+    const props: GetUserUseCase.Input = {
+      id: entity.id,
+    }
+
+    const output = await sut.execute(props)
+
+    expect(output).toEqual(entity.toJSON())
+  })
+})
