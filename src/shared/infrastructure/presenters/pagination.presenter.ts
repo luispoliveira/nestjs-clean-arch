43599016d5df951419e3f92ae12b3ac454c8a2fb
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-11 12:09:03 +0100

    feat: add PaginationPresenter for handling pagination data serialization

---
+++ src/shared/infrastructure/presenters/pagination.presenter.ts
@@ -0,0 +1,25 @@
+import { Transform } from 'class-transformer'
+
+export type PaginationPresenterProps = {
+  currentPage: number
+  perPage: number
+  lastPage: number
+  total: number
+}
+export class PaginatioPresenter {
+  @Transform(({ value }) => parseInt(value))
+  currentPage: number
+  @Transform(({ value }) => parseInt(value))
+  perPage: number
+  @Transform(({ value }) => parseInt(value))
+  lastPage: number
+  @Transform(({ value }) => parseInt(value))
+  total: number
+
+  constructor(props: PaginationPresenterProps) {
+    this.currentPage = props.currentPage
+    this.perPage = props.perPage
+    this.lastPage = props.lastPage
+    this.total = props.total
+  }
+}
