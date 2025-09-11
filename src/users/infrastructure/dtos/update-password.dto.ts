f64463161137ae70bb15191215efb470e9853552
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-11 23:17:18 +0100

    feat: add validation decorators to DTOs for improved data integrity

--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,8 +1,14 @@
 import { UpdatePasswordUseCase } from '@/users/application/usecases/update-password.usecase'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 export class UpdatePasswordDto
   implements Omit<UpdatePasswordUseCase.Input, 'id'>
 {
+  @IsString()
+  @IsNotEmpty()
   oldPassword: string
+
+  @IsString()
+  @IsNotEmpty()
   password: string
 }
