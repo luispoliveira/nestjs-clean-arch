551c581f2a8ffbf2be52df8d7bff2852f492db6d
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-11 15:18:46 +0100

    test: add end-to-end tests for user creation in UsersController

---
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -0,0 +1,63 @@
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { PrismaClient } from '@generated/prisma'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import request from 'supertest'
+import { SignUpDto } from '../../dtos/sign-up.dto'
+import { UsersModule } from '../../users.module'
+
+describe('UsersController e2e tests', () => {
+  let app: INestApplication
+  let module: TestingModule
+  let repository: UserRepository.Repository
+  let signUpDto: SignUpDto
+  const prismaService = new PrismaClient()
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile()
+    app = module.createNestApplication()
+    await app.init()
+    repository = module.get<UserRepository.Repository>('UserRepository')
+  })
+
+  beforeEach(async () => {
+    signUpDto = {
+      email: 'test@example.com',
+      password: 'password',
+      name: 'Test User',
+    }
+    await prismaService.user.deleteMany()
+  })
+
+  describe('POST /users', () => {
+    it('should create a user', async () => {
+      await request(app.getHttpServer())
+        .post('/users')
+        .send(signUpDto)
+        .expect(201)
+        .then(async response => {
+          const { body } = response
+          expect(body).toHaveProperty('id')
+          expect(body).toHaveProperty('email', signUpDto.email)
+          expect(body).toHaveProperty('name', signUpDto.name)
+          expect(body).not.toHaveProperty('password')
+
+          const userInDb = await repository.findById(body.id)
+          expect(userInDb).not.toBeNull()
+          expect(userInDb?.email).toBe(signUpDto.email)
+          expect(userInDb?.name).toBe(signUpDto.name)
+          expect(userInDb?.password).not.toBe(signUpDto.password) // Password should be hashed
+        })
+    })
+  })
+})
