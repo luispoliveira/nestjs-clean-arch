6e8b43db4f4ac8969c1bd0910f180b2a166ea49b
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-17 14:41:13 +0100

    test: remove commented-out tests for user validation in UsersController e2e tests

--- src/shared/infrastructure/exception-filters/not-found-error/__tests__/e2e/not-found-error.filter.e2e-spec.ts
+++ src/shared/infrastructure/exception-filters/not-found-error/__tests__/e2e/not-found-error.filter.e2e-spec.ts
@@ -1,7 +1,41 @@
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { Controller, Get, INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import request from 'supertest'
 import { NotFoundErrorFilter } from '../../not-found-error.filter'
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new NotFoundError('NotFound error example')
+  }
+}
+describe('NotFoundErrorFilter (e2e)', () => {
+  let app: INestApplication
+  let module: TestingModule
 
-describe('NotFoundErrorFilter', () => {
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile()
+    app = module.createNestApplication()
+    app.useGlobalFilters(new NotFoundErrorFilter())
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
   it('should be defined', () => {
     expect(new NotFoundErrorFilter()).toBeDefined()
+  })
+
+  it('should return a not found error response', async () => {
+    return request(app.getHttpServer()).get('/stub').expect(404).expect({
+      statusCode: 404,
+      message: 'NotFound error example',
+      error: 'Not Found',
+    })
   })
 })
--- src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
@@ -88,58 +88,5 @@ describe('UsersController e2e tests', () => {
           message: 'User with id fakeId not found',
         })
     })
-
-    // it('should return a error with 422 code when the email field is invalid', async () => {
-    //   const { email, ...signUpDtoWithoutEmail } = signUpDto
-    //   const res = await request(app.getHttpServer())
-    //     .post('/users')
-    //     .send({ ...signUpDtoWithoutEmail })
-    //     .expect(422)
-
-    //   expect(res.body.error).toBe('Unprocessable Entity')
-    //   expect(res.body.message).toEqual([
-    //     'email must be an email',
-    //     'email should not be empty',
-    //     'email must be a string',
-    //   ])
-    // })
-
-    // it('should return a error with 422 code when the password field is invalid', async () => {
-    //   const { password, ...signUpDtoWithoutPassword } = signUpDto
-    //   const res = await request(app.getHttpServer())
-    //     .post('/users')
-    //     .send({ ...signUpDtoWithoutPassword })
-    //     .expect(422)
-
-    //   expect(res.body.error).toBe('Unprocessable Entity')
-    //   expect(res.body.message).toEqual([
-    //     'password should not be empty',
-    //     'password must be a string',
-    //   ])
-    // })
-
-    // it('should return a error with 422 code with invalid field provided', async () => {
-    //   const res = await request(app.getHttpServer())
-    //     .post('/users')
-    //     .send(Object.assign(signUpDto, { xpto: 'fake' }))
-    //     .expect(422)
-    //   expect(res.body.error).toBe('Unprocessable Entity')
-    //   expect(res.body.message).toEqual(['property xpto should not exist'])
-    // })
-
-    // it('should return a error with 409 code when email is already taken', async () => {
-    //   const entity = new UserEntity(UserDataBuilder({ ...signUpDto }))
-    //   await repository.insert(entity)
-
-    //   const res = await request(app.getHttpServer())
-    //     .post('/users')
-    //     .send(signUpDto)
-    //     .expect(409)
-    //     .expect({
-    //       statusCode: 409,
-    //       error: 'Conflict',
-    //       message: 'User with email test@example.com already exists',
-    //     })
-    // })
   })
 })
