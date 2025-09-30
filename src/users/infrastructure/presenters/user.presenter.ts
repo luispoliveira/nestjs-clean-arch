fbc58af4f8d3234937db18c35d820318d93e6076
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-30 23:36:03 +0100

    feat: add Swagger API properties to User and UserCollection presenters for improved documentation

--- src/users/infrastructure/presenters/user.presenter.ts
+++ src/users/infrastructure/presenters/user.presenter.ts
@@ -1,10 +1,24 @@
 import { UserOutputDTO } from '@/users/application/dtos/user-output.dto'
+import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
 
 export class UserPresenter {
+  @ApiProperty({ example: 'asdf', description: 'User unique identifier' })
   id: string
+
+  @ApiProperty({ example: 'John Doe', description: 'User full name' })
   name: string
+
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    description: 'User email address',
+  })
   email: string
+
+  @ApiProperty({
+    example: '2023-01-01T00:00:00.000Z',
+    description: 'User creation date',
+  })
   @Transform(({ value }: { value: Date }) => value.toISOString())
   createdAt: Date
 
