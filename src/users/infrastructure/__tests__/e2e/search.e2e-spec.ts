d051570a21531bd2776493205772d2a632b03f6e
Luis Oliveira <luispedroliveira96@gmail.com>
2025-09-30 17:14:23 +0100

    test: add e2e tests for retrieving users ordered by createdAt in UsersController

---
+++ src/users/infrastructure/__tests__/e2e/search.e2e-spec.ts
@@ -0,0 +1,93 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { applyGlobalConfig } from '@/global-config'
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
+import { EnvConfigModule } from '@/shared/infrastructure/env-config/env-config.module'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserRepository } from '@/users/domain/repositories/user.repository'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { PrismaClient } from '@generated/prisma'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import { instanceToPlain } from 'class-transformer'
+import request from 'supertest'
+import { UsersController } from '../../users.controller'
+import { UsersModule } from '../../users.module'
+
+describe('UsersController e2e tests', () => {
+  let app: INestApplication
+  let module: TestingModule
+  let repository: UserRepository.Repository
+  const prismaService = new PrismaClient()
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [
+        EnvConfigModule,
+        UsersModule,
+        DatabaseModule.forTest(prismaService),
+      ],
+    }).compile()
+    app = module.createNestApplication()
+    applyGlobalConfig(app)
+    await app.init()
+    repository = module.get<UserRepository.Repository>('UserRepository')
+  })
+
+  beforeEach(async () => {
+    await prismaService.user.deleteMany()
+  })
+
+  describe('GET /users', () => {
+    it('should return the users ordered by createdAt', async () => {
+      const createdAt = new Date()
+      const entities: UserEntity[] = []
+
+      const arrange = Array(3).fill(UserDataBuilder({}))
+      arrange.forEach((item, index) => {
+        createdAt.setSeconds(createdAt.getSeconds() + index + 1)
+        const entity = new UserEntity({
+          ...item,
+          createdAt: createdAt,
+          email: `user${index}@example.com`,
+        })
+
+        entities.push(entity)
+      })
+
+      await prismaService.user.createMany({
+        data: entities.map(i => i.toJSON()),
+      })
+
+      const searchParams = {}
+      const queryParams = new URLSearchParams(searchParams as any).toString()
+
+      const res = await request(app.getHttpServer())
+        .get(`/users?${queryParams}`)
+        .expect(200)
+
+      console.log('🚀 ~ res:', res.body)
+
+      expect(Object.keys(res.body)).toStrictEqual(['data', 'meta'])
+      expect(res.body).toStrictEqual({
+        data: [...entities]
+          .reverse()
+          .map(item =>
+            instanceToPlain(UsersController.userToResponse(item as any)),
+          ),
+        meta: {
+          currentPage: 1,
+          perPage: 15,
+          total: 3,
+          lastPage: 1,
+        },
+      })
+      // const user = await repository.findById(entity.id)
+      // const presenter = UsersController.userToResponse(user.toJSON())
+      // const serialized = instanceToPlain(presenter)
+
+      // expect(res.body.data).toEqual(serialized)
+    })
+  })
+})
